1ef7ded1633660a725ee573ec8eafc361ce733c3
Thiruparan <thirupparan1994@gmail.com>
2019-05-15 20:58:50 +0530

    üser role update button disable feature done

---
+++ DB_ALTER.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `user_role` ADD UNIQUE( `role_name`);
--- userrole_fetch.php
+++ userrole_fetch.php
@@ -48,17 +48,19 @@ foreach($result as $row)
 	{
 		$status = '<span class="label label-success">Active</span>';
 		$statusCheck='<input type="checkbox" name="delete" checked id="'.$row["role_id"].'" class="delete"  data-status="'.$row["role_status"].'">';
+		$updatebutton='<button type="button" name="update" id="'.$row["role_id"].'" class="btn btn-warning btn-xs update">Update</button>';
 	}
 	else
 	{
 		$status = '<span class="label label-danger">Inactive</span>';
 		$statusCheck='<input type="checkbox" name="delete" id="'.$row["role_id"].'" class="delete" data-status="'.$row["role_status"].'">';
+		$updatebutton='<button type="button" disabled name="update" id="'.$row["role_id"].'" class="btn btn-warning btn-xs update">Update</button>';
 	}
 	$sub_array = array();
 	$sub_array[] = $row['role_id'];
 	$sub_array[] = $row['role_name'];
 	$sub_array[] = $status;
-	$sub_array[] = '<button type="button" name="update" id="'.$row["role_id"].'" class="btn btn-warning btn-xs update">Update</button>';
+	$sub_array[] = $updatebutton;
 	$sub_array[] = $statusCheck;
 	$data[] = $sub_array;
 }
